766ab169228e6328e5d19ede2ca968b86701ab1e
mjb3102 <mblair@sjfc.edu>
2018-07-30 14:17:57 -0400

    Changed Footer
    Made the footer display correct information

--- footer.php
+++ footer.php
@@ -15,17 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sjfcdoc' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'sjfcdoc' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'sjfcdoc' ), 'sjfcdoc', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<p>Copyright © 2018 St. John Fisher College • 3690 East Avenue, Rochester, New York 14618 • 585.385.8000</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
