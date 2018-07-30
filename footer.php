1146e7674de356fdb6d84b4e1e84c2b30924b0a8
Maryann Reissig <mreissig@sjfc.edu>
2018-07-30 16:43:15 -0400

    v1.1.1

--- footer.php
+++ footer.php
@@ -11,15 +11,15 @@
 
 ?>
 
-	</div><!-- #content -->
+	
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<p id="footer-info">Copyright &copy; <?php echo date(Y) ?> St. John Fisher College • 3690 East Avenue, Rochester, New York 14618 • 585.385.8000</p>
+			<hr /><p id="footer-info">Copyright &copy; <?php echo date(Y) ?> St. John Fisher College • 3690 East Avenue, Rochester, New York 14618 • 585.385.8000</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
-
+</div><!-- #content -->
 <?php wp_footer(); ?>
 
 </body>
