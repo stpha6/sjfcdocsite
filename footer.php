30bf0b4b06293de630c8547eeb0ed3b301989a4c
mjb3102 <mblair@sjfc.edu>
2018-07-30 14:32:57 -0400

    Make Copyright Year a PHP Function
    Make Copyright Year a PHP Function

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<p>Copyright © 2018 St. John Fisher College • 3690 East Avenue, Rochester, New York 14618 • 585.385.8000</p>
+			<p id=“footer-info”>Copyright &copy; <?php echo date(“Y”) ?> St. John Fisher College • 3690 East Avenue, Rochester, New York 14618 • 585.385.8000</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
