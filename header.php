b7f932daa20dca90a29031e3158438a0be35207c
Maryann Reissig <mreissig@sjfc.edu>
2018-07-31 16:15:10 -0400

    added google analytics

--- header.php
+++ header.php
@@ -29,6 +29,17 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 	<?php wp_head(); ?>
+	
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123152553-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-123152553-1');
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
